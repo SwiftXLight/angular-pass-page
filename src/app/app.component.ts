5e555e59600f1cb4e010aeaa7b92babe1a82f41b
Andrii Panchenko <andrevpanchenko@gmail.com>
2023-07-21 19:52:31 +0300

    feat: add correct colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-pass-page';
+  password: string = '';
+  passwordStrength: 'empty-pass' | 'short' | 'weak' | 'medium' | 'strong' = 'empty-pass';
+  minPasswordLength: number = 8;
+
+  checkPasswordStrength(): void {
+    const hasLetters = this.password.match(/[a-zA-Z]/) !== null;
+    const hasDigits = this.password.match(/[0-9]/) !== null;
+    const hasSymbols = this.password.match(/[!@#$%^&*]/) !== null;
+
+    if (hasLetters && hasDigits && hasSymbols) {
+      this.passwordStrength = 'strong';
+    } else if ((this.password.length < this.minPasswordLength) && (this.password.length > 0)) {
+      this.passwordStrength = 'short';
+    } else if ((hasLetters && hasDigits) || (hasDigits && hasSymbols) || (hasLetters && hasSymbols)) {
+      this.passwordStrength = 'medium';
+    } else if (this.password.length === 0) {
+      this.passwordStrength = 'empty-pass';
+    } else {
+      this.passwordStrength = 'weak';
+    }
+  }
 }
